2b5adfb8cc42e6820aad8abbda7c00db255f4d5c
tuitstudent <tuitstudent2000@gmail.com>
2025-02-12 15:47:22 +0500

    version updated

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Electron + React + Auto Updater (1.0.1) version started</h1>
+      <h1>Electron + React + Auto Updater (1.0.2) version started</h1>
       <p>Bu ilova avtomatik yangilanishni qo‘llab-quvvatlaydi.</p>
     </div>
   );
